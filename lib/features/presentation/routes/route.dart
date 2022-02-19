1cdf4b593c8c563c87d82aa3773df8978e64b97a
zackmolt <fkrihkl@gmail.com>
2022-02-19 21:59:15 +0700

    fix some writing style

--- lib/features/presentation/routes/route.dart
+++ lib/features/presentation/routes/route.dart
@@ -1,14 +1,12 @@
 
-import 'package:easy_cook/features/presentation/pages/detail/SeeAll.dart';
 import 'package:easy_cook/features/presentation/pages/detail/detail_page.dart';
-import 'package:easy_cook/features/presentation/pages/detail/searchpage.dart';
+import 'package:easy_cook/features/presentation/pages/search/search_page.dart';
 import 'package:easy_cook/features/presentation/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 const String homePage = 'home_page';
 const String detailPage = 'detail_page';
 const String searchPage = 'search_page';
-const String seeAll = 'see_all';
 
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
@@ -23,10 +21,6 @@ Route<dynamic> controller(RouteSettings settings) {
     case searchPage:
       return MaterialPageRoute(
           builder: (context) => SearchPage()
-      );
-    case seeAll:
-      return MaterialPageRoute(
-          builder: (context) => SeeAll()
       );
 
     default:
